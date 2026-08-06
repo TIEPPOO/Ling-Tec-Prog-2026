0e2a8adce350d1ad3d7b1a7cd42256ab08eb4ef1
Enderson TIEPPO <endersontieppo@gmail.com>
2026-08-05 22:25:50 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	float a,b,c;
+	float resultado; 
+	
+	a = 10;
+	b = 5;
+	c = 9;
+	
+	resultado = (a+b+c)/3;
+	
+	printf("resultado %f",resultado);
+	
+	return 0;
+	
+}
