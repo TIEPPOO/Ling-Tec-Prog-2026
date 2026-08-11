4672c3f16c2510d172a60edcb4520ebdfbc4d95d
Enderson TIEPPO <endersontieppo@gmail.com>
2026-08-10 22:32:19 -0300

    Implement circle area calculation in CalculodeCirculo.c
    Added functionality to calculate the area of a circle based on user input for the radius.

---
+++ CalculodeCirculo.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define pi 3.14159
+
+int main(int argc, char *argv[]) {
+// area do circulo
+	
+	float raio, area, raioQuadrado;
+	
+	printf("Insira o valor de R: \n");
+	scanf("%f", &raio);
+	
+	raioQuadrado = raio*raio;
+	area = pi * raioQuadrado;
+	
+	printf("A area do circulo de raio %f = %f", raio, area);
+	
+	// trapezio
+	 
+	 float b1, b2, h area2;
+	 
+	 printf ("Insira 1 valor \n");
+	 scanf("%f", &b1);
+	 
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
