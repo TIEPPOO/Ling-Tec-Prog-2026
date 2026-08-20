0b0727cb6fcf16d54764a7bbee87f48e67b0c169
Enderson TIEPPO <endersontieppo@gmail.com>
2026-08-19 21:49:39 -0300

    Add program to compare three integers
    Implement a program to find the largest of three integers using absolute value.

---
+++ Listx2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////// EX 10
+
+  int a,b,c, maior_temp, maior;
+	printf("Insira os valores a serem comparados: ");
+	scanf("%d %d %d",&a, &b, &c);
+	
+	maior_temp = ((a+b)+abs(a-b))/2; ////// ABS: valor absoluto de um número inteiro
+	maior = ((maior_temp+c)+abs(maior_temp-c))/2;
+	
+	printf("O maior entre |%d|%d|%d| = %d", a, b, c, maior);
+	
+	
+	return 0;
+}
