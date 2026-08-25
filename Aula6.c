a3eb27ff997be598b9592caa19cd1bf4826c404d
Enderson TIEPPO <endersontieppo@gmail.com>
2026-08-24 22:20:33 -0300

    Update inverse calculation logic in Aula6.c
    Fix input handling and inverse calculation for negative numbers.

---
+++ Aula6.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	
+	/*
+	
+	int a,b,c;
+	int resultado;
+	
+	printf("Insira os valores de A, B, C: ");
+	scanf("%d %d %d", &a, &b, &c);
+	
+	if (a>b){
+		resultado = a;
+	}else{
+		resultado = b;
+	}
+	
+	if (c>resultado){
+		resultado = c;
+	}
+	
+	printf("%d e maior ", resultado);
+	
+	*/
+	
+	
+	int n, inverso;
+	
+	printf("Insira os valor:");
+	scanf("&d", n);
+	
+	
+	
+	if (n<0){
+	
+	inverso = 1 / n;
+	
+ } 
+ 
+ 
+ 
+ printf("%d o valor e:", inverso);
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
