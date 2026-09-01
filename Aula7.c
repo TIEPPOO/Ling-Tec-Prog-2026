f1118167df5bcba7bc105871a220601a102fcf60
Enderson TIEPPO <endersontieppo@gmail.com>
2026-08-31 22:24:47 -0300

    Create Aula7.c

---
+++ Aula7.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* crie um programa que leia um numero, entre 1 e 9 e caso ele seja positivo, verifique se ele é um numero primo, caso seja, imprima seu dobro, caso  não seja primo, verifique se é par ou impar. Se for
+par mostre o resultado da soma com 2. Se for impar mostre seu proximo e seu anterior. caso seja negativo mostre seu inverso. Caso não esteja entre 1 e 9 mostre_out_of_range */
+
+int main(int argc, char *argv[]) {
+	
+/*	int n;
+	printf("Entre com o numero: ");
+	scanf("%d", &n);
+	    // equivalente (n>=1 && n <-10)
+	if (n<10 && n>0)
+		  // isso é interresante (n==4 || n==6 || n==8 || n==9)
+		if (n==1 || n==2 || n==3 || n==5 || n==7)
+			printf("O dobro  de %d = %d", n, (n*n));
+		else if (n%2 == 0)
+			printf("%d+2 =  %d", n, n+2);
+		else
+			printf("|%d|%d|%d|", n-1, n, n+1);
+		
+	else printf("out _of_range"); */
+	
+	
+
+	
+	char op;
+	scanf("%c" &op);
+	switch(op){
+	
+	case 'a' : printf("opa ta falando com ele"); break;	
+	
+	case 'b' : printf("benedito"); break;
+	
+	case 'c' : printf("È complicada a situação"); break;
+	
+	case 'd' : printf("tamo junto"); break;
+	}
+	
+	return 0;
+}
