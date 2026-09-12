cd4a1df5e58703bb8615501f75ba7e2fb0e247b7
Enderson TIEPPO <endersontieppo@gmail.com>
2026-09-12 19:08:21 -0300

    Update Lista1.c

--- Lista1.c
+++ Lista1.c
@@ -19,5 +19,40 @@
 	
 	printf("%lfx10\n", notacao);
 
+////////////////////// TERCEIRO /////////////////
+
+int main(int argc, char *argv[]) {
+	
+int n, res;
+	int bit_64, bit_32, bit_16, bit_8, bit_4, bit_2;
+	
+	printf("Insira o valor: ");
+	scanf("%d", &n);
+	
+	bit_64 = n%2; /////% resto da divisão
+	res = n/2; //// sempre calcular na mão
+	
+	bit_32 = res%2;
+	res = res/2;
+	
+	bit_16 = res%2;
+	res = res/2;
+	
+	bit_8 = res%2;
+	res = res/2;
+	
+	bit_4 = res%2;
+	res = res/2;
+	
+	bit_2 = res%2;
+	res = res/2;
+	
+	printf("O numero %d em binario = %d%d%d%d%d%d%d", n, res%2,bit_2, bit_4, bit_8, bit_16,bit_32, bit_64);
+
+
+
+
+////////////////////////// QUARTA ///////////////////////////////////
+
 
 
